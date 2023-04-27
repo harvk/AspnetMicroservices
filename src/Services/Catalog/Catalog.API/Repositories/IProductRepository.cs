aabe878ea1b91b85d041f6635638546881d14ecd
King P <k.harvey324@yahoo.com>
2023-04-27 17:38:12 -0400

    Added API project, MongoDb setup, added Mongo image to docker-compose

---
+++ src/Services/Catalog/Catalog.API/Repositories/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using Catalog.API.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<List<Product>> GetProducts(); // must use List<>, IEnumerable<> causes bugs
+        Task<Product> GetProduct(string id);
+        Task<List<Product>> GetProductByName(string name);
+        Task<List<Product>> GetProductByCategroy(string categoryName);
+        Task CreateProduct(Product product);
+        Task<bool> UpdateProduct(Product product);
+        Task<bool> DeleteProduct(string id);
+    }
+}
